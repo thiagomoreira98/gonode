84da195384de173a79bbfbcff8e7a3822596e6c1
Thiago Moreira <moreira.g.thiago@gmail.com>
2019-11-25 14:17:06 -0300

    up

---
+++ app/Models/User.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Model = use('Model')
+
+/** @type {import('@adonisjs/framework/src/Hash')} */ // intellisense
+const Hash = use('Hash')
+
+class User extends Model {
+  static boot () {
+    super.boot()
+
+    this.addHook('beforeSave', async (userInstance) => {
+      if (userInstance.dirty.password) {
+        userInstance.password = await Hash.make(userInstance.password)
+      }
+    })
+  }
+
+  tokens () {
+    return this.hasMany('App/Models/Token')
+  }
+
+  projects () {
+    return this.hasMany('App/Models/Project')
+  }
+
+  tasks () {
+    return this.hasMany('App/Models/Task')
+  }
+}
+
+module.exports = User
